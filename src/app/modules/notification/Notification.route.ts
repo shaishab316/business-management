c4e40680fada28eb2525cafc53557069c496c599
shaishabcoding <shaishabchandrashil@gmail.com>
2025-08-07 15:29:11 +0600

    feat: Nest notification templates route under notification route
    This commit nests the notification templates route under the main notification route for better organization.

--- src/app/modules/notification/Notification.route.ts
+++ src/app/modules/notification/Notification.route.ts
@@ -3,16 +3,20 @@ import { NotificationControllers } from './Notification.controller';
 import { NotificationValidations } from './Notification.validation';
 import purifyRequest from '../../middlewares/purifyRequest';
 import { NotificationTemplateValidations } from '../notificationTemplate/NotificationTemplate.validation';
+import { NotificationTemplateRoutes } from '../notificationTemplate/NotificationTemplate.route';
 
 const subAdmin = Router();
+{
+  subAdmin.use('/templates', NotificationTemplateRoutes);
 
-subAdmin.post(
-  '/send',
-  purifyRequest(
-    NotificationValidations.send,
-    NotificationTemplateValidations.create,
-  ),
-  NotificationControllers.send,
-);
+  subAdmin.post(
+    '/send',
+    purifyRequest(
+      NotificationValidations.send,
+      NotificationTemplateValidations.create,
+    ),
+    NotificationControllers.send,
+  );
+}
 
 export const NotificationRoutes = { subAdmin };
